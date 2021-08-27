0a3eb5762ed561c77a95b07e7ab196e3301fffbd
akanhodev <akanho.dev@gmail.com>
2021-08-27 13:18:04 +0100

    started swipped coding

---
+++ src/components/SwipeButtons.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReplayIcon from '@material-ui/icons/Replay';
+import CloseIcon from '@material-ui/icons/Close';
+import StarRateIcon from '@material-ui/icons/StarRate';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FlashOnIcon from '@material-ui/icons/FlashOn';
+import IconButton from '@material-ui/core/IconButton';
+
+import './SwipeButtons.css'
+
+function SwipeButtons() {
+    return (
+        <div className="swipeButtons">
+            {/* <h1>Buttons go there !!!!</h1> */}
+            < ReplayIcon fontSize='large'/>
+            < CloseIcon fontSize='large'/>
+            < StarRateIcon fontSize='large'/>
+            < FavoriteIcon fontSize='large'/>
+            < FlashOnIcon fontSize='large'/>
+
+        </div>
+    )
+}
+
+export default SwipeButtons
